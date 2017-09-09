c99c514646dccae54c7beb930c5e7959c0c6ac32
Matthew Gilbert <matthew.gilbert12@gmail.com>
2017-09-09 18:26:51 -0400

    fixing references to mapper

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import re
 
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package#7071358
-VERSIONFILE = "mapper/_version.py"
+VERSIONFILE = "mapping/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -13,30 +13,30 @@ else:
                        (VERSIONFILE,))
 
 LONG_DESCRIPTION = """
-`mapper` provides functionality for mapping to and from generic exposures and
+`mapping` provides functionality for mapping to and from generic exposures and
 tradeable instruments for financial assets.
 
 An example of this might be on 2016-12-01 we would have `CL1 -> CLZ16`, i.e.
 the first generic for Crude oil on the above date corresponds to trading the
 December 2016 contract.
 
-The main features of `mapper` include:
+The main features of `mapping` include:
 
 - creating continuous return series for Futures instruments
 - creating time series of percentage allocations to tradeable contracts
 - creating instrument trade lists
 """
 
-setup(name='mapper',
+setup(name='mapping',
       version=verstr,
       description='Mappings for generic and tradeable futures instruments',
       long_description=LONG_DESCRIPTION,
-      url='https://github.com/MatthewGilbert/mapper',
+      url='https://github.com/MatthewGilbert/mapping',
       author='Matthew Gilbert',
       author_email='matthew.gilbert12@gmail.com',
       license='MIT',
       platforms='any',
       install_requires=['pandas', 'numpy', 'cvxpy'],
-      packages=['mapper', 'mapper.tests'],
-      test_suite='mapper.tests',
+      packages=['mapping', 'mapping.tests'],
+      test_suite='mapping.tests',
       zip_safe=False)
