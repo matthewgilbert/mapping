bcd7e40c3a75524874f388d50476fa2786b2dd37
Matthew Gilbert <matthew.gilbert12@gmail.com>
2018-05-21 20:13:04 -0400

    Refactor assertRaises unit tests

--- mapping/tests/test_util.py
+++ mapping/tests/test_util.py
@@ -306,23 +306,20 @@ class TestUtil(unittest.TestCase):
         assert_series_equal(res, res_exp)
 
     def test_to_notional_duplicates(self):
-        def duplicate_error(instrs, prices, mults, *args):
-            return util.to_notional(instrs, prices, mults, *args)
-
         instrs = pd.Series([1, 1], index=['A', 'A'])
         prices = pd.Series([200.37], index=['A'])
         mults = pd.Series([100], index=['A'])
-        self.assertRaises(ValueError, duplicate_error, instrs, prices, mults)
+        self.assertRaises(ValueError, util.to_notional, instrs, prices, mults)
 
         instrs = pd.Series([1], index=['A'])
         prices = pd.Series([200.37, 200.37], index=['A', 'A'])
         mults = pd.Series([100], index=['A'])
-        self.assertRaises(ValueError, duplicate_error, instrs, prices, mults)
+        self.assertRaises(ValueError, util.to_notional, instrs, prices, mults)
 
         instrs = pd.Series([1], index=['A'])
         prices = pd.Series([200.37], index=['A'])
         mults = pd.Series([100, 100], index=['A', 'A'])
-        self.assertRaises(ValueError, duplicate_error, instrs, prices, mults)
+        self.assertRaises(ValueError, util.to_notional, instrs, prices, mults)
 
         instrs = pd.Series([1], index=['A'])
         prices = pd.Series([200.37], index=['A'])
@@ -330,7 +327,7 @@ class TestUtil(unittest.TestCase):
         desired_ccy = "CAD"
         instr_fx = pd.Series(['USD', 'USD'], index=['A', 'A'])
         fx_rate = pd.Series([1.32], index=['USDCAD'])
-        self.assertRaises(ValueError, duplicate_error, instrs, prices, mults,
+        self.assertRaises(ValueError, util.to_notional, instrs, prices, mults,
                           desired_ccy, instr_fx, fx_rate)
 
         instrs = pd.Series([1], index=['A'])
@@ -339,7 +336,7 @@ class TestUtil(unittest.TestCase):
         desired_ccy = "CAD"
         instr_fx = pd.Series(['USD'], index=['A'])
         fx_rate = pd.Series([1.32, 1.32], index=['USDCAD', 'USDCAD'])
-        self.assertRaises(ValueError, duplicate_error, instrs, prices, mults,
+        self.assertRaises(ValueError, util.to_notional, instrs, prices, mults,
                           desired_ccy, instr_fx, fx_rate)
 
     def test_to_contracts_rounder(self):
